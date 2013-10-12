62c51740a643d4ab6e58bf48225e469e68d6840c
Alberto La Rocca <a71104@gmail.com>
2013-10-12 08:29:29 +0200

    Three.js JSON format parser

--- Modules.js
+++ Modules.js
@@ -2,11 +2,106 @@ function Modules(assets) {
 	var program = assets.getProgram('base');
 	var modules = assets.getData('level.json').modules;
 
+	function readArrays(mesh) {
+		var count = 0;
+		var vertices = [];
+		var uvs = [];
+
+		function pushVertex(index) {
+			vertices.push(
+				mesh.vertices[index * 3],
+				mesh.vertices[index * 3 + 1],
+				mesh.vertices[index * 3 + 2],
+				1
+				);
+		}
+
+		function pushUV(index) {
+			uvs.push(
+				mesh.uvs[index * 2],
+				mesh.uvs[index * 2 + 1]
+				);
+		}
+
+		var i = 0;
+		while (i < mesh.faces.length) {
+			var type = mesh.faces[i];
+			if (type & 1) {
+				pushVertex(mesh.faces[i + 1]);
+				pushVertex(mesh.faces[i + 2]);
+				pushVertex(mesh.faces[i + 3]);
+				pushVertex(mesh.faces[i + 3]);
+				pushVertex(mesh.faces[i + 4]);
+				pushVertex(mesh.faces[i + 1]);
+				i += 4;
+				count += 6;
+			} else {
+				pushVertex(mesh.faces[i + 1]);
+				pushVertex(mesh.faces[i + 2]);
+				pushVertex(mesh.faces[i + 3]);
+				i += 3;
+				count += 3;
+			}
+			if (type & 2) {
+				i++;
+			}
+			if (type & 4) {
+				i++;
+			}
+			if (type & 8) {
+				if (type & 1) {
+					pushUV(mesh.faces[i + 1]);
+					pushUV(mesh.faces[i + 2]);
+					pushUV(mesh.faces[i + 3]);
+					pushUV(mesh.faces[i + 3]);
+					pushUV(mesh.faces[i + 4]);
+					pushUV(mesh.faces[i + 1]);
+					i += 4;
+				} else {
+					pushUV(mesh.faces[i + 1]);
+					pushUV(mesh.faces[i + 2]);
+					pushUV(mesh.faces[i + 3]);
+					i += 3;
+				}
+			} else {
+				if (type & 1) {
+					uvs.push(0, 0, 0, 0, 0, 0);
+				} else {
+					uvs.push(0, 0, 0);
+				}
+			}
+			if (type & 16) {
+				i++;
+			}
+			if (type & 32) {
+				if (type & 1) {
+					i += 4;
+				} else {
+					i += 3;
+				}
+			}
+			if (type & 64) {
+				i++;
+			}
+			if (type & 128) {
+				if (type & 1) {
+					i += 4;
+				} else {
+					i += 3;
+				}
+			}
+		}
+
+		var arrays = new oogl.AttributeArrays(count);
+		arrays.add4f(vertices);
+		arrays.add2f(uvs);
+		return arrays;
+	}
+
 	var arrayMap = {};
 	modules.forEach(function (module) {
 		if (!arrayMap[module.path]) {
-			var mesh = assets.getData(module.path);
-			// TODO
+			moduleArrays[module.path] = readArrays(assets.getData('modules/' + module.path));
 		}
 	});
 
